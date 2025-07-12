dbc06e3d4fd2798f46b5f0576679a578b719a4e8
Bisbilge <bisbilge@gmail.com>
2025-07-13 00:41:38 +0300

    Clean and reorder migrations to fix user-product dependency issues

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Recipe, RecipeIngredient
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'weight', 'is_edible', 'energy', 'decay_time')
+    search_fields = ('name',)
+    list_filter = ('is_edible',)
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('product', 'output_quantity', 'craft_time')
+    inlines = [RecipeIngredientInline]
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity')
+    list_filter = ('recipe',)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Inventory, ProductionExpertise
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Economy Info', {
+            'fields': ('money', 'max_carry_weight')
+        }),
+    )
+    list_display = ('username', 'email', 'money', 'max_carry_weight', 'is_staff')
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity')
+    list_filter = ('product',)
+    search_fields = ('user__username',)
+
+@admin.register(ProductionExpertise)
+class ProductionExpertiseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'level', 'xp')
+    list_filter = ('product',)
+    search_fields = ('user__username',)
